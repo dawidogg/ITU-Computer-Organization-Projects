7bac33b49d90625286148162370ec55fe88e5323
dawidogg <dawidogg.feed@gmail.com>
2023-03-29 20:32:47 +0300

    Add part 1

---
+++ Project1/part1.v
@@ -0,0 +1,25 @@
+module register #(
+  parameter NBits = 16
+) (
+  input[1:0]          funsel, 
+  input               e, 
+  input[NBits-1: 0]   i,
+  output[NBits-1: 0]  q
+);
+
+reg[NBits-1:0] data;
+assign q = data;
+
+initial data = {NBits{1'b0}};
+
+always @(funsel or e or i) begin
+  if (e)
+    case (funsel)
+      2'b00: data = {NBits{1'b0}};  // Clear
+      2'b01: data = i;              // Load
+      2'b10: data = data - 1'b1;    // Decrement
+      2'b11: data = data + 1'b1;    // Increment
+    endcase
+end 
+
+endmodule
---
+++ Project1/part1_tb.v
@@ -0,0 +1,55 @@
+`include "part1.v"
+
+module register_tb;
+  reg[3:0] input_4;
+  wire[3:0] output_4;
+  reg[1:0] funsel;
+  reg enable;
+  
+  register #(.NBits(4)) register_4  (
+    .funsel (funsel         ),
+    .e      (enable         ),
+    .i      (input_4        ),
+    .q      (output_4       )
+  );
+
+  task displayHead;
+    $display("%6s %6s %6s %6s","Enable","FunSel","Input","Output");
+  endtask
+
+  initial begin
+    $display("Testing 4-bit register \n");
+    input_4 = 4'b0;
+    funsel = 2'b0;
+    enable = 1'b0;
+    
+    $monitor("%6b %6b %6b %6b", enable, funsel, input_4, output_4);
+    
+    $display("Load & clear test:");
+    displayHead();
+    enable = 1'b1;
+    funsel = 2'b01; input_4 = 4'b1111; #5; 
+    funsel = 2'b00; #5; 
+    funsel = 2'b01; input_4 = 4'b1010; #5; 
+    funsel = 2'b00; #5; 
+    funsel = 2'b01; input_4 = 4'b0001; #5; 
+    funsel = 2'b00; #5; 
+    funsel = 2'b01; input_4 = 4'b0110; #5; 
+    funsel = 2'b00; #5; 
+    funsel = 2'b01; input_4 = 4'b0000; #5; 
+    funsel = 2'b00; #5; 
+    
+    $display("\nIncrement test");
+    displayHead();
+    funsel = 2'b00; #5;
+    funsel = 2'b11;
+    repeat(32) #5 enable = ~enable;
+
+    $display("\nDecrement test");
+    displayHead();
+    funsel = 2'b00; #5;
+    funsel = 2'b10;
+    repeat(32) #5 enable = ~enable;
+  end
+
+endmodule
