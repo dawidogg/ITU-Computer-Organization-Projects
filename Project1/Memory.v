a4d48eacae9cb54813a459d9995c76f558bb7f2b
dawidogg <dawidogg.feed@gmail.com>
2023-03-29 17:36:40 +0300

    First commit

---
+++ Project1/Memory.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/19/2023 12:25:11 PM
+// Design Name: 
+// Module Name: Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Memory(
+    input wire[7:0] address,
+    input wire[7:0] data,
+    input wire wr, //Read = 0, Write = 1
+    input wire cs, //Chip is enable when cs = 0
+    input wire clock,
+    output reg[7:0] o // Output
+);
+    //Declaration oýf the RAM Area
+    reg[7:0] RAM_DATA[0:255];
+    //Read Ram data from the file
+    initial $readmemh("RAM.mem", RAM_DATA);
+    //Read the selected data from RAM
+    always @(*) begin
+        o = ~wr && ~cs ? RAM_DATA[address] : 8'hZ;
+    end
+    
+    //Write the data to RAM
+    always @(posedge clock) begin
+        if (wr && ~cs) begin
+            RAM_DATA[address] <= data; 
+        end
+    end
+endmodule
---
+++ README.md
@@ -0,0 +1,12 @@
+# ITU Computer Organization Projects
+
+* Team members: Denıs Iurıe Davıdoglu (150200916), Matay Aydın (150200075), Mustafa Kırcı (150200096).
+
+* Project 1 task distribution:
+  - Part 1 - Denis
+  - Part 2a - 
+  - Part 2b - 
+  - Part 2c - 
+  - Part 3 - 
+  - Part 4 - 
+  - Report - 
